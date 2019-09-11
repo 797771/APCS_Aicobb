c3d08c7e438abc51c047d85dda391282327f4ba2
Isabelle Cobb <797771@seq.org>
2019-09-10 20:15:24 -0700

    Lab9-10 athome edits(9/10)

--- Labs/Lab9-10/MMMRunner.java
+++ Labs/Lab9-10/MMMRunner.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.lang.Math;
 /**
  * MMMRunner
  *
@@ -8,42 +9,52 @@ import java.util.Arrays;
 public class MMMRunner
 {
     int[] nums = new int[100]; //create array nums
-    
+
     /**
      * Constructor for objects of class MMMRunner
      */
     public MMMRunner()
     {
-        
+
     }
-    
+
     //picks random numbers for array nums
     public void loadArray(){
-        for(int i=0; i<nums.length; i++){
-            nums[i] = (int) (Math.random()*10) + 1;
+        for(int i=0; i < nums.length; i++){
+            nums[i] = (int)(Math.random()*10) + 1;
         }
     }
-    
-    
-    //come back to this
+
+    //prints out array in lines of 10
     public void printArray(){
-        for(int i=0; i<nums.length; i++){
-            
+        for(int index = 9; index<100; index+=10){
+            for(int i=index-9; i<=index; i++){
+                if(i<index){
+                    System.out.print(nums[i] + ", ");
+                }
+
+                if(i==index & i!= 99){
+                    System.out.println(nums[i] + ", ");
+                }
+
+                if(i==index & i==99){
+                    System.out.println(nums[i]);
+                }
+            }
         }
     }
-    
-    
+
     //returns sum of numbers in array
     public int getSum(){
         int sum = 0;
         for(int i=0; i<nums.length;i++){
             sum = sum + nums[i];
         }
-        
-        return sum;
+
+        System.out.println(sum);
+        return(sum);
     }
-    
-    
+
     //gives mean value of array
     public double getMean(){
         int sum = 0;
@@ -51,18 +62,16 @@ public class MMMRunner
             sum = sum + nums[i];
         }
         
+        System.out.println(sum/nums.length);
         return (sum/nums.length);
     }
-    
+
     //gives median value of array
     public double getMedian(){
-        Arrays.sort();
+        Arrays.sort(nums, 0, nums.length);
         double median = (nums[49] + nums[50])/2;
-        return(median);
-    }
-    
-    //gives mode value of array
-    public int getMode(){
         
+        System.out.print(median);
+        return(median);
     }
 }
