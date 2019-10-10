defaecdd1b756a097d7d7c60efba5fd19a750a42
Isabelle Cobb <797771@seq.org>
2019-10-09 17:50:31 -0700

    At home
    10/9/19

--- Test Review/Test 2 Review/Test.java
+++ Test Review/Test 2 Review/Test.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Test
  *
@@ -7,7 +7,7 @@
  */
 public class Test
 {
-
+    
     /**
      * Constructor for objects of class Test
      */
@@ -17,6 +17,22 @@ public class Test
     }
 
     public static void main(){
-
+        int[] evens = new int[4];
+        
+        int index = 0;
+        while(index<4){
+            int random = (int)(Math.random()*7)+2;
+            while(random % 2 != 0){
+                random = (int)(Math.random()*7)+2;
+            }
+            if(random % 2 ==0){
+                evens[index] = random;
+            }
+            System.out.print(evens[index] + ", ");
+            index++;
+        }
+        
+        
     }
+
 }
