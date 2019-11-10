ea964ed19a4a31be620b86465c2c87d272e0becc
Isabelle Cobb <797771@seq.org>
2019-11-10 15:56:36 -0800

    Finished Bench 3 on StudList Proj
    11/10/19

--- Projects/StudentList/StudentList.java
+++ Projects/StudentList/StudentList.java
@@ -14,7 +14,7 @@ public class StudentList
      */
     public StudentList()
     {
-       
+
     }
 
     /**
@@ -40,7 +40,7 @@ public class StudentList
 
         System.out.print("Student GPA: ");
         inputGPA = kb.nextDouble();
-        
+
         Student studListOb = new Student(inputName, inputNum, inputGPA);
         studList.add(studListOb);
 
@@ -51,34 +51,180 @@ public class StudentList
      * Does not print null records
      */
     public void printList(){
-
         for(int i = 0; i<studList.size(); i++){
             if(studList.get(i) != null){
-                System.out.println("Student Name: " + studList.get(i).getName() + ", Student Number: " + studList.get(i).getNum() + ", Student GPA: " + studList.get(i).getGPA());
+                System.out.println("Student Name: " + studList.get(i).getName() + ", Student Number: " + 
+                    studList.get(i).getNum() + ", Student GPA: " + studList.get(i).getGPA());
             }
         }
 
+        if(studList.size() <=0){
+            System.out.print("\nThere are no students currently in the Student list.");
+        }
     }
-    
+
     /*
      * Allows the user to specify a Student last name deleted from the list.  
      */
     public void deleteStudentFromList(String lName){
-        
+
         for(int i = 0; i<studList.size();i++){
             if(lName.equals(studList.get(i).getLastName())){
                 studList.remove(i);
             }
         }
-        
+
     }
-    
+
     /*
      *   Allows the user to specify a student number and deletes that student from the list.
      */
-    //public void deleteStudentFromList(int stuNumber){
-        //stuNumber = getStudID;
-        
-    //}
+    public void deleteStudentFromList(int stuNumber){
+        for(int i = 0; i<studList.size();i++){
+            if(stuNumber == studList.get(i).getNum()){
+                studList.remove(i);
+            }
+        }
+    }
+
+    /*
+     * Deletes the entire list (null in each element without a student record)
+     */
+    public void clearList(){
+        studList.clear();
+        if(studList.size() <=0){
+            System.out.print("\nThere are no students currently in the Student list.");
+        }
+    }
+
+    /*
+     * Clears  the screen and then prints a student with the input name to the screen.  
+     * If the Student does not exist, print message to the console: "Student does not exist."
+     */
+    public void printStudent(String lastName){
+        int existVal = 0;
+        for(int i = 0; i<studList.size();i++){
+            if(lastName.equals(studList.get(i).getLastName())){
+                System.out.println("Student Name: " + studList.get(i).getName() + ", Student Number: " + 
+                    studList.get(i).getNum() + ", Student GPA: " + studList.get(i).getGPA());
+            }
+            if(!lastName.equals(studList.get(i).getLastName())){
+                existVal++;
+            }
+        }
+
+        if(existVal == studList.size()){
+            System.out.println("\nStudent does not exist.");
+        }
+    }
+
+    /*
+     * Clears  the screen and then prints a student student with the input student number to the screen.  
+     * If the Student does not exist, print message to the console: "Student does not exist."
+     */
+    public void printStudent(int stuNumber){
+        int existVal = 0;
+        for(int i = 0; i<studList.size();i++){
+            if(stuNumber == studList.get(i).getNum()){
+                System.out.println("Student Name: " + studList.get(i).getName() + ", Student Number: " + 
+                    studList.get(i).getNum() + ", Student GPA: " + studList.get(i).getGPA());
+            }
+            if(stuNumber != studList.get(i).getNum()){
+                existVal++;
+            }
+        }
+
+        if(existVal == studList.size()){
+            System.out.println("\nStudent does not exist.");
+        }
+    }
+
+    /*
+     * Sorts students by student name
+     */
+    public void sortStudentsName(){
+        for(int i = 1; i<studList.size(); i++){//insertion sort
+            int j = i;
+            while(j>0 && (studList.get(j).getLastName().compareTo(studList.get(j-1).getLastName()))<0)
+            {
+                swapList(studList, j, j-1);
+                j--;
+            }
+        }
+
+    }
+
+    /*
+     * Sorts students by student number,
+     * want to put object with smallest student number at 
+     * front of list
+     */
+    public void sortStudentsNum(){
+        for(int i = 1; i<studList.size(); i++){//insertion sort
+            int j = i;
+            while(j>0 && (studList.get(j).getNum() < 
+                studList.get(j-1).getNum())){
+                swapList(studList, j, j-1);
+                j--;
+            }
+        }
+    }
+
+    //swapping method for different sorting methods
+    public void swapList(ArrayList<Student> tempList, int i, int n){
+        Student temp = tempList.get(i);
+        tempList.set(i, tempList.get(n));
+        tempList.set(n, temp);
+    }
+
+    /*
+     * Allows the user to specify a student name and then update or change the Student name and GPA
+     */
+    public void editStudentList(String lastName){
+        for(int i = 0; i<studList.size();i++){
+            if(lastName.equals(studList.get(i).getLastName())){
+                System.out.println("Student Name: " + studList.get(i).getName() + ", Student Number: " + 
+                    studList.get(i).getNum() + ", Student GPA: " + studList.get(i).getGPA());
+
+                Scanner kb2 = new Scanner(System.in);
+                String newName = "";
+                double newGPA;
+
+                System.out.print("Student name: ");
+                newName = kb2.nextLine();
+
+                System.out.print("Student GPA: ");
+                newGPA = kb2.nextDouble();
+
+                Student studListTemp = new Student(newName, studList.get(i).getNum(), newGPA);
+                studList.set(i, studListTemp);
+            }
+        }
+    }
+
+    /*
+     * Allows the user to specify a Student number and then update or change the Student name and GPA
+     */
+    public void editStudentList(int stuNumber){
+        for(int i = 0; i<studList.size();i++){
+            if(stuNumber == studList.get(i).getNum()){
+                System.out.println("Student Name: " + studList.get(i).getName() + ", Student Number: " + 
+                    studList.get(i).getNum() + ", Student GPA: " + studList.get(i).getGPA());
+
+                Scanner kb2 = new Scanner(System.in);
+                String newName = "";
+                double newGPA;
+
+                System.out.print("Student name: ");
+                newName = kb2.nextLine();
+
+                System.out.print("Student GPA: ");
+                newGPA = kb2.nextDouble();
+
+                Student studListTemp = new Student(newName, studList.get(i).getNum(), newGPA);
+                studList.set(i, studListTemp);
+            }
+        }
+    }
 
 }
--- Projects/StudentList/StudentListRunner.java
+++ Projects/StudentList/StudentListRunner.java
@@ -25,22 +25,78 @@ public class StudentListRunner
             System.out.println("4. Search for a Student\n5. Clear Student List\n6. Sort Student List");
             System.out.println("7. Print one Student\n8. Edit Student List");
             inputStr = kb.nextLine();
-            
-            if(inputStr.equals("1")){
+
+            if(inputStr.equals("1")){//add student
                 studList.addStudent();
             }
-            if(inputStr.equals("2")){
-                System.out.print("\nStudent's Last Name: ");
-                String inputLName = kb.nextLine();
-                studList.deleteStudentFromList(inputLName);
+            if(inputStr.equals("2")){//delete student
+                System.out.print("\nEnter \"1\" to delete a student by last name. Enter \"2\" to delete a student by student number.");
+                int inputVal = kb.nextInt();
+
+                Scanner kb2 = new Scanner(System.in);
+                if(inputVal == 1){
+                    System.out.print("\nStudent's Last Name: ");
+                    String inputLName = kb2.nextLine();
+                    studList.deleteStudentFromList(inputLName);
+                }
+                if(inputVal == 2){
+                    System.out.print("\nStudent's student number: ");
+                    int inputNum = kb2.nextInt();
+                    studList.deleteStudentFromList(inputNum);
+                }
             }
-            if(inputStr.equals("3")){
+            if(inputStr.equals("3")){//print studentlist
                 studList.printList();
             }
-            
+            if(inputStr.equals("5")){//clear studentlist
+                studList.clearList();
+            }
+            if(inputStr.equals("6")){//sort studentlist
+                System.out.print("\nEnter \"1\" to sort the student list by last name. Enter \"2\" to sort the student list by student number.");
+                int inputVal = kb.nextInt();
+
+                if(inputVal == 1){
+                    studList.sortStudentsName();
+                }
+                if(inputVal == 2){
+                    studList.sortStudentsNum();
+                }
+            }
+            if(inputStr.equals("7")){//print one student
+                System.out.print("\nEnter \"1\" to print a student by last name. Enter \"2\" to print a student by student number.");
+                int inputVal = kb.nextInt();
+
+                Scanner kb2 = new Scanner(System.in);
+                if(inputVal == 1){
+                    System.out.print("\nStudent's Last Name: ");
+                    String inputLName = kb2.nextLine();
+                    studList.printStudent(inputLName);
+                }
+                if(inputVal == 2){
+                    System.out.print("\nStudent's student number: ");
+                    int inputNum = kb2.nextInt();
+                    studList.printStudent(inputNum);
+                }
+            }
+            if(inputStr.equals("8")){//edit studentlist
+                System.out.print("\nEnter \"1\" to edit the student list by last name. Enter \"2\" to edit the student list by student number.");
+                int inputVal = kb.nextInt();
+                
+                Scanner kb2 = new Scanner(System.in);
+                if(inputVal == 1){
+                    System.out.print("\nStudent's Last Name: ");
+                    String inputLName = kb2.nextLine();
+                    studList.editStudentList(inputLName);
+                }
+                if(inputVal == 2){
+                    System.out.print("\nStudent's student number: ");
+                    int inputNum = kb2.nextInt();
+                    studList.editStudentList(inputNum);
+                }
+            }
         }
     }
-    
+
     public static void main(){
         StudentListRunner studListRun = new StudentListRunner();
     }
